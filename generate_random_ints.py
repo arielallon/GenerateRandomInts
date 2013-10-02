36fc4aa75a87b5ac0c605bdcd162dff6f0e0ab34
Ariel Allon <arielallon@gmail.com>
2013-10-01 21:51:41 -0500

    removed commented out old version

--- generate_random_ints.py
+++ generate_random_ints.py
@@ -14,17 +14,3 @@ while (count < total):
 		print num
 		generated.set(num)
 		count += 1
-
-# write a file with all the numbers
-# numbers = xrange(10000000, 99999999)
-# for number in numbers:
-# 	print number
-
-# disadvantage is that removing elements is likely very slow.
-# setting them to null as we go could result in plenty of false positives til we get them all
-# num_remaining = len(numbers) - 1
-# while num_remaining > 0:
-# 	index = random.randint(0,num_remaining)
-# 	print numbers[index]
-# 	del(numbers[index])
-# 	num_remaining -= 1
